9f2d924e94a1d760a3ace8c4c820043afc42eb7e
Takayuki Konishi <tkonishi@redhat.com>
2017-01-25 17:23:10 +0900

    Modify test method name

--- test/test.js
+++ test/test.js
@@ -2,11 +2,8 @@ var assert = require('assert');
 var mediator = require('fh-wfm-mediator/lib/mediator');
 var nsInspector = require('../lib/ns-inspector');
 
-describe('Array', function () {
-    it('should return -1 when the value is not present', function (done) {
-        mediator.subscribe('wfm:user:create', function () { console.log('foo')});
-        nsInspector.inspect(mediator);
-        assert.equal(1, 1);
-        done();
-    });
+it('should output 4 namespaces', function (done) {
+    mediator.subscribe('wfm:user:create', function () { console.log('foo') });
+    nsInspector.inspect(mediator);
+    done();
 });
